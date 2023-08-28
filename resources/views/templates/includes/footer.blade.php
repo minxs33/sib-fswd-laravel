54b5362902206c2c0759c8deadb4e9db3a4604dd
minxs33 <theminxs12@gmail.com>
2023-08-28 15:45:38 +0700

    [add] Adding onsite footer change in BNSP sertification interview

--- resources/views/templates/includes/footer.blade.php
+++ resources/views/templates/includes/footer.blade.php
@@ -1,3 +1,32 @@
-<footer class="py-3 bg-success">
-    <p class="text-center text-white p-0">© Naufal Fadhilah Alwan, 4612408</p>
-</footer>
+<div class="bg-white">
+    <div class="container">
+    <footer class="py-5">
+        <div class="row">
+            <div class="col-6 col-md-6 col-sm-12 mb-3">
+                <h5 class="fw-bold text-success">Vision</h5>
+                <p class="ps-3">Naufal Alwan Merch aims to be a leading online store that provides a variety of high-quality merchandise for loyal fans. We want to be the main destination for lovers of unique and exclusive products that combine creativity, attractive designs and the best quality. By keeping innovation and customer satisfaction in mind, we are committed to providing an extraordinary shopping experience and being a trusted partner for the entire community of merchandise enthusiasts around the world.</p>
+            </div>
+
+            <div class="col-6 col-md-6 col-sm-12 mb-3">
+                <h5 class="fw-bold text-success">Mission</h5>
+                <ol class="flex-column">
+                    <li class="mb-2"><a class="p-0 text-muted text-decoration-none">Providing High Quality Products.</a></li>
+                    <li class="mb-2"><a class="p-0 text-muted text-decoration-none">Offering Unique and Exclusive Merchandise.</a></li>
+                    <li class="mb-2"><a class="p-0 text-muted text-decoration-none">Providing a Pleasant Shopping Experience.</a></li>
+                    <li class="mb-2"><a class="p-0 text-muted text-decoration-none">Build an Active Fan Community.</a></li>
+                    <li class="mb-2"><a class="p-0 text-muted text-decoration-none">Contributing to Environmental Conservation.</a></li>
+                </ol>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p class="text-success-emphasis">© 2023 Naufal Alwan Merch. All rights reserved.</p>
+        <ul class="list-unstyled d-flex">
+            <li class="ms-3"><a class="link-dark" target="_blank" href="https://www.twitter.com"><i class="fab fa-twitter"></i></a></li>
+            <li class="ms-3"><a class="link-dark" target="_blank" href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
+            <li class="ms-3"><a class="link-dark" target="_blank" href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+        </div>
+    </footer>
+    </div>
+</div>
